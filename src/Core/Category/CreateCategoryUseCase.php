86142fa2dd8350905c4ba04470cde7ce609b819c
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-19 15:15:14 -0400

    s0206 - Iniciando com o TDD
    O que foi feito:
    - Princípio da segregação de interfaces

---
+++ src/Core/Category/CreateCategoryUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Category;
+
+class CreateCategoryUseCase
+{
+    private $repository;
+
+    public function __construct(CategoryRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+}
