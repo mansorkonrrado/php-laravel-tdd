820aa51ef6bf59dd0a2d01ecd92f0ae72e5b66fb
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-19 12:09:03 -0400

    s02e03 - Iniciando com o TDD
    O que foi feito:
    - PHPUnit e TDD Mudar a Classe de Produto

--- tests/Unit/Core/Orders/ProductUnitTest.php
+++ tests/Unit/Core/Orders/ProductUnitTest.php
@@ -7,9 +7,24 @@ use PHPUnit\Framework\TestCase;
 
 class ProductUnitTest extends TestCase
 {
+    public function testAttributes()
+    {
+        $product = new Product(
+            id: '1',
+            name: 'Product xpto',
+            price: 10,
+            total: 5
+        );
+
+        $this->assertEquals('1', $product->getId());
+        $this->assertEquals('Product xpto', $product->getName());
+        $this->assertEquals(10, $product->getPrice());
+    }
+
     public function testCalc()
     {
         $product = new Product(
+            id: '1',
             name: "Product xpto",
             price: 10,
             total: 12
@@ -21,6 +36,7 @@ class ProductUnitTest extends TestCase
     public function testCalcWithTax()
     {
         $product = new Product(
+            id: '1',
             name: "Product xpto",
             price: 100,
             total: 2
