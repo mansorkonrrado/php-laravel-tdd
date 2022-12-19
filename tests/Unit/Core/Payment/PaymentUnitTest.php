e6a985bbed386fbb8ddbe0124ed3d5b26116d52a
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-19 17:22:48 -0400

    s02e09 - Iniciando com o TDD
    O que foi feito:
    - O Padrão Triple A (Arrange, Act, Assert)

--- tests/Unit/Core/Payment/PaymentUnitTest.php
+++ tests/Unit/Core/Payment/PaymentUnitTest.php
@@ -10,23 +10,35 @@ use stdClass;
 
 class PaymentUnitTest extends TestCase
 {
+    // constructor class Test
+    // protected function setUp(): void
+    // {
+
+    // }
+
     public function testPayment()
     {
+        // arrange
         $mockPayment = Mockery::mock(stdClass::class, PaymentInterface::class);
         $mockPayment->shouldReceive('makePayment')
             ->times(1)
             ->andReturn(true);
 
         $payment = new PaymentController($mockPayment);
+
+        // act
         $response = $payment->execute();
 
+        // assert 
         $this->assertTrue($response);
-    }
-
-    protected function tearDown(): void
-    {
         Mockery::close();
-
-        parent::tearDown();
     }
+
+    // destructor class Test
+    // protected function tearDown(): void
+    // {
+    //     Mockery::close();
+
+    //     parent::tearDown();
+    // }
 }
