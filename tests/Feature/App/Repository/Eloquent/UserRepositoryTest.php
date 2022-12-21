4140a52f2d3e40ded689d9c499cdea132f8240ee
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-21 16:32:41 -0400

    s05e03 - Testes de Integração
    O que foi feito:
    - Testes de Integração no Laravel - findAll

--- tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
+++ tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
@@ -10,16 +10,22 @@ use Tests\TestCase;
 
 class UserRepositoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_find_all_empty()
     {
         $repository = new UserRepository(new User());
         $response = $repository->findAll();
 
         $this->assertIsArray($response);
+        $this->assertCount(0, $response);
+    }
+
+    public function test_find_all()
+    {
+        User::factory()->count(10)->create();
+
+        $repository = new UserRepository(new User());
+        $response = $repository->findAll();
+
+        $this->assertCount(10, $response);
     }
 }
