1add3a9f4ba4d96b2956245638c047c0547b6a1a
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-23 11:44:36 -0400

    s06e02 - Testes E2E
    O que foi feito:
    - Teste E2E no Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', function () {
-    return response()->json([]);
-});
+Route::get('/users', [UserController::class, 'index']);
