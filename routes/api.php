46630920e0e8b2ecd35bbcb4df60605419dd4c38
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-28 11:19:44 -0400

    s06e19 - Testes E2E
    O que foi feito:
    - Testes E2E no Laravel - Deletar Usuário

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::delete('/users/{email}', [UserController::class, 'destroy']);
 Route::put('/users/{email}', [UserController::class, 'update']);
 Route::get('/users/{email}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
