041c80db9351903f2b70c2c53ef489ca6032fce7
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-28 10:28:43 -0400

    s06e18 - Testes E2E
    O que foi feito:
    - Testes E2E no Laravel - Not Found Update

--- tests/Feature/Api/UserApiTest.php
+++ tests/Feature/Api/UserApiTest.php
@@ -160,4 +160,12 @@ class UserApiTest extends TestCase
             ],
         ];
     }
+
+    public function test_update_not_found()
+    {
+        $response = $this->putJson("{$this->endpoint}/fake_value", [
+            'name' => 'Konrrado Mansor'
+        ]);
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
 }
