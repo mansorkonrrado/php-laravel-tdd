0f7e2445fa3aea7bf88dd7a2f0267d2cc8efed91
Konrrado Mansor <konrrado.mansor@gmail.com>
2022-12-28 11:30:07 -0400

    s07e01 - Melhorias Finais
    O que foi feito:
    - Melhorar Rotas API no Laravel

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/users/{email}', [UserController::class, 'destroy']);
-Route::put('/users/{email}', [UserController::class, 'update']);
-Route::get('/users/{email}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users', [UserController::class, 'index']);
+/**
+ * Exemplo didático:
+ * A rota Resource entende automaticamente os verbos HTTP de cada requisição. Ela simpĺifica a escrita de todas as demais rotas.
+ * Logo abaixo, no bloco comentado, são as rotas individuais com seus verbos HTTP. Poderíamos trabalhar com elas também.
+ */
+Route::apiResource('/users', UserController::class);
+// Route::delete('/users/{email}', [UserController::class, 'destroy']);
+// Route::put('/users/{email}', [UserController::class, 'update']);
+// Route::get('/users/{email}', [UserController::class, 'show']);
+// Route::post('/users', [UserController::class, 'store']);
+// Route::get('/users', [UserController::class, 'index']);
